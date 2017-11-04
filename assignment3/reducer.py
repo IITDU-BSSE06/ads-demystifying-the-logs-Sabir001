08c88eb4b826da948c68de4a72585123e0a15f91
Sharafat Ahmed Sabir <s.a.sabir@hotmail.com>
2017-11-04 17:00:45 +0600

    reducer updated

--- assignment3/reducer.py
+++ assignment3/reducer.py
@@ -8,4 +8,4 @@ for line in sys.stdin:
 	file_path = line.strip()
 	dictionaryCounter[file_path] = dictionaryCounter.get(file_path, 0) + 1
 maxFile = max(dictionaryCounter, key=dictionaryCounter.get)
-print maxFile + " " + str(dictionaryCounter.get(maxFile, 0))
+print maxFile
