cb0e67989195b9075589dadd12719102bf67708d
Sharafat Ahmed Sabir <s.a.sabir@hotmail.com>
2017-11-08 12:50:54 +0600

    mapper updated for problem with get

--- assignment4/mapper.py
+++ assignment4/mapper.py
@@ -5,9 +5,10 @@ from urlparse import urlparse
 import sys
 
 for line in sys.stdin:
-    data = line.strip().split("GET")
-    if len(data) == 2:
-        rightSide = data[1]
+    data = line.strip().split(" ")
+    if len(data) == 10:
+        ip, hyphen1, hyphen2, date, number, req, filePath, protocol, status, port = data
+        rightSide = filePath
         file_path = urlparse(rightSide.split()[0]).path
         if file_path:
             print str(file_path)
